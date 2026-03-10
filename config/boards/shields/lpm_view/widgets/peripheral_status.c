df2e3af3ba457f71fcf970ec9342f8db3293159b
Leedaiguu <104456518+Leedaiguu@users.noreply.github.com>
2026-03-10 15:24:46 +0900

    Update peripheral_status.c

--- config/boards/shields/lpm_view/widgets/peripheral_status.c
+++ config/boards/shields/lpm_view/widgets/peripheral_status.c
@@ -1,8 +1,6 @@
 /*
- *
  * Copyright (c) 2023 The ZMK Contributors
  * SPDX-License-Identifier: MIT
- *
  */
 
 #include <zephyr/kernel.h>
@@ -22,7 +20,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/wpm.h>
 
 #include "peripheral_status.h"
-#include "util.h"
+
+/* util.h는 peripheral_status.h에서 이미 include됨 */
 
 LV_IMG_DECLARE(spaceship1);
 LV_IMG_DECLARE(spaceship2);
@@ -82,97 +81,6 @@ static void draw_stars(struct art_state *s) {
         lv_canvas_draw_rect(canvas, s->star2_x[i], s->star2_y[i], 2, 2, &star);
     }
 }
-
-static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], const struct status_state *state) {
-
-    lv_obj_t *canvas = lv_obj_get_child(widget, 0);
-
-    lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &lv_font_montserrat_16, LV_TEXT_ALIGN_RIGHT);
-
-    lv_draw_rect_dsc_t rect_black_dsc;
-    init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
-
-    lv_canvas_draw_rect(canvas, 0, 0, CANVAS_SIZE, CANVAS_SIZE, &rect_black_dsc);
-
-    draw_battery(canvas, state);
-
-    lv_canvas_draw_text(canvas, 0, 0, CANVAS_SIZE, &label_dsc,
-                        state->connected ? LV_SYMBOL_WIFI : LV_SYMBOL_CLOSE);
-
-    rotate_canvas(canvas, cbuf);
-}
-
-static void set_battery_status(struct zmk_widget_status *widget,
-                               struct battery_status_state state) {
-
-#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-    widget->state.charging = state.usb_present;
-#endif
-
-    widget->state.battery = state.level;
-
-    draw_top(widget->obj, widget->cbuf, &widget->state);
-}
-
-static void battery_status_update_cb(struct battery_status_state state) {
-
-    struct zmk_widget_status *widget;
-
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
-        set_battery_status(widget, state);
-    }
-}
-
-static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
-
-    return (struct battery_status_state){
-        .level = zmk_battery_state_of_charge(),
-#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-        .usb_present = zmk_usb_is_powered(),
-#endif
-    };
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_status, struct battery_status_state,
-                            battery_status_update_cb, battery_status_get_state)
-
-ZMK_SUBSCRIPTION(widget_battery_status, zmk_battery_state_changed);
-
-#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
-#endif
-
-static struct peripheral_status_state get_state(const zmk_event_t *eh) {
-
-    return (struct peripheral_status_state){
-        .connected = zmk_split_bt_peripheral_is_connected()
-    };
-}
-
-static void set_connection_status(struct zmk_widget_status *widget,
-                                  struct peripheral_status_state state) {
-
-    widget->state.connected = state.connected;
-
-    draw_top(widget->obj, widget->cbuf, &widget->state);
-}
-
-static void output_status_update_cb(struct peripheral_status_state state) {
-
-    struct zmk_widget_status *widget;
-
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
-        set_connection_status(widget, state);
-    }
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_peripheral_status,
-                            struct peripheral_status_state,
-                            output_status_update_cb,
-                            get_state)
-
-ZMK_SUBSCRIPTION(widget_peripheral_status, zmk_split_peripheral_status_changed)
 
 static void art_anim_timer_cb(lv_timer_t *timer) {
 
