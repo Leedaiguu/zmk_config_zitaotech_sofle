a12dded7024f1b20e90c96c6c7c0317379dba325
Leedaiguu <104456518+Leedaiguu@users.noreply.github.com>
2026-03-10 14:21:30 +0900

    Create spaceship1.c

---
+++ config/boards/shields/lpm_view/widgets/bunnygirl_anima/spaceship1.c
@@ -0,0 +1,34 @@
+#include <lvgl.h>
+
+#ifndef LV_ATTRIBUTE_MEM_ALIGN
+#define LV_ATTRIBUTE_MEM_ALIGN
+#endif
+
+#ifndef LV_ATTRIBUTE_IMG_SPACESHIP1
+#define LV_ATTRIBUTE_IMG_SPACESHIP1
+#endif
+
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SPACESHIP1 uint8_t
+spaceship1_map[] = {
+
+#if CONFIG_NICE_VIEW_WIDGET_INVERTED
+0xff,0xff,0xff,0xff,
+0x00,0x00,0x00,0xff,
+#else
+0x00,0x00,0x00,0xff,
+0xff,0xff,0xff,0xff,
+#endif
+
+/* BITMAP_DATA */
+
+};
+
+const lv_img_dsc_t spaceship1 = {
+.header.cf = LV_IMG_CF_INDEXED_1BIT,
+.header.always_zero = 0,
+.header.reserved = 0,
+.header.w = 120,
+.header.h = 72,
+.data_size = 1088,
+.data = spaceship1_map,
+};
